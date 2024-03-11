5b9af3cce8f0246e623f2337b8adfaf68c518a90
K0nny16 <kowal.karl99@gmail.com>
2024-03-11 09:57:19 +0100

    Initial commit

---
+++ src/main/java/com/gritacademyapi/Java23WebServicesGritAcademyApiKarlKowalApplication.java
@@ -0,0 +1,13 @@
+package com.gritacademyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java23WebServicesGritAcademyApiKarlKowalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java23WebServicesGritAcademyApiKarlKowalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gritacademyapi/Java23WebServicesGritAcademyApiKarlKowalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gritacademyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java23WebServicesGritAcademyApiKarlKowalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
