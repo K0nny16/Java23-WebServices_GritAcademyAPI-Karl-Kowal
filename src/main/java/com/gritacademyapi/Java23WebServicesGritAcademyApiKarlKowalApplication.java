b4f92bbc6ca56cf5fb85dc02b30fac1bc88bcc34
Karl Kowal <kowal.karl99@gmail.com>
2024-03-15 00:17:02 +0100

    Att göra grejer

--- src/main/java/com/gritacademyapi/Java23WebServicesGritAcademyApiKarlKowalApplication.java
+++ src/main/java/com/gritacademyapi/Java23WebServicesGritAcademyApiKarlKowalApplication.java
@@ -10,3 +10,23 @@ public class Java23WebServicesGritAcademyApiKarlKowalApplication {
 		SpringApplication.run(Java23WebServicesGritAcademyApiKarlKowalApplication.class, args);
 	}
 }
+
+/*
+		Att Göra
+
+1:Felhantera ifall det är en tom json osv.
+2:Kommentera koden och kolla upp saker för att underlätta presentation.
+3:Backend för punkt 4.
+4:Påbörja jsp sidorna som ska användas för VG (From för att lägga till kurs, elev och relation mellan dom 2)
+
+
+		VG Punkter
+		
+Lägga till kurser.
+Tabort kurs med ID.
+Lägga till elev.
+Tabort elev med ID.
+Lägga till relation mellan kurs och elev.
+Tabort relation mellan kurs och elev med ID.
+
+ */
